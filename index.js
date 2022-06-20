c13cc404cbb414a38d014bd805fc5a11abe08ad1
WINDOWS 10 <haliferliawan4@gmail.com>
2022-06-20 17:28:28 +0700

    commite kedua

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
-const importData = require("./data.json")
+const importDataPesawat = require("./dataPesawat.json")
+const importDataHotel = require("./dataHotel.json")
 let port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
     res.send("Hello World")
 });
 
-app.get("/data", (req, res) => {
-    res.send(importData);
+app.get("/dataPesawat", (req, res) => {
+    res.send(importDataPesawat);
 });
+
+app.get("/dataHotel", (req,res)=> {
+    res.send(importDataHotel);
+})
 
 app.listen(port, () => {
     console.log(`Sukses Berjalan Di PORT http://localhost:${port}`);
